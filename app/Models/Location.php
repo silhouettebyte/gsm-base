22dd67e8ef439ddbb4b2e3d3ab49f950ae65d981
Arki Valencia II <arkiv2.rog@gmail.com>
2021-05-13 15:11:50 +0800

    Fix Location

--- app/Models/Location.php
+++ app/Models/Location.php
@@ -10,4 +10,12 @@ class Location extends Model
     use HasFactory;
 
     protected $fillable = ['rssi', 'gnss', 'network', 'geoloc'];
+
+    public function getGeolocAttribute($value)
+	{
+		$geo = explode(',', $value);
+		$geo[0] = round($geo[0], 5);
+		$geo[1] = round($geo[1], 5);
+		return implode(',', $geo);
+	}
 }
